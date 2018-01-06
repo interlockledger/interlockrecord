b9a1833e05ca0294adcc37bd52e2abcf674169ef
Cesar Ferraz <cferraz@opencs.com.br>
2018-01-05 23:28:21 -0200

    lib updated with all methods

--- src/irecord/include/irecord/irecord.h
+++ src/irecord/include/irecord/irecord.h
@@ -29,6 +29,7 @@
 
 #include <irecord/irdll.h>
 #include <irecord/irerr.h>
+#include <stdint.h>
 
 /**
  * @defgroup irecord_pub Public Functions
@@ -165,8 +166,381 @@ IR_EXPORT_ATTR int IR_EXPORT_CALL IRGetVersionInt(int * major, int * minor);
 *  @{
 */
 
+/**
+* Creates an emergency key as an integer.
+*
+* @param[in] context The new context.
+* @param[in] type The type of the emergency key.
+* @param[in] size The size of the emergency key.
+* @param[out] hkey The created emergency key.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IREmergencyKeyCreate(IRContext context, int type, int size, int * hKey);
+
+/**
+* Disposes an emergency key.
+*
+* @param[in] context The new context.
+* @param[in] hkey The emergency key.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IREmergencyKeyDispose(IRContext context, int hKey);
+
+/**
+* Serializes an emergency key.
+*
+* @param[in] context The new context.
+* @param[in] hkey The emergency key.
+* @param[out] buffSize The buffer size of the serialized emergency key.
+* @param[out] buff The emergency key serialized buffer.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IREmergencyKeySerialized(IRContext context, int hKey, int ** buffSize, void * buff);
+
+/**
+* Loads an emergency key.
+*
+* @param[in] context The new context.
+* @param[in] buffSize The buffer size of the emergency key.
+* @param[in] buff The emergency key buffer.
+* @param[out] hkey The loaded emergency key.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IREmergencyKeyLoad(IRContext context, int buffSize, void * buff, int * hKey);
 
 /** @}*/ //addtogroup irecord_pub_emergency_key
+
+//==============================================================================
+// Root Record Template
+//------------------------------------------------------------------------------
+/**
+* @defgroup irecord_pub_root_record_template Root Record Template
+*/
+/** @addtogroup irecord_pub_root_record_template
+*  @{
+*/
+
+/**
+* Creates a new Root Record Template.
+*
+* @param[in] context The new context.
+* @param[out] hTemplate The root record template created.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRRootTemplateCreate(IRContext context, int * hTemplate);
+
+/**
+* Disposes a Root Record Template.
+*
+* @param[in] context The new context.
+* @param[in] hTemplate The root record template to be disposed.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRRootTemplateDispose(IRContext context, int hTemplate);
+
+/**
+* Root Record Template Parameter.
+*
+* @param[in] context The new context.
+* @param[in] hTemplate The root record template.
+* @param[in] param The new root record template parameter.
+* @param[in] value The root record template parameter value.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRRootTemplateParam(IRContext context, int hTemplate, int param, const char * value);
+
+/**
+* Sets a Parent Root Record Template.
+*
+* @param[in] context The new context.
+* @param[in] hTemplate The root record template.
+* @param[in] parentRootRecord The parent root record.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRRootTemplateSetParent(IRContext context, int hTemplate, int parentRootRecord);
+
+/**
+* Sets an emergency key in a root record template.
+*
+* @param[in] context The new context.
+* @param[in] hTemplate The root record template.
+* @param[in] hKey The emergency key to be set.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRRootTemplateSetEmergencyKey(IRContext context, int hTemplate, int hKey);
+
+/**
+* Sets a new network to the root record template.
+*
+* @param[in] context The new context.
+* @param[in] hTemplate The root record template.
+* @param[in] networkID The network ID to be set.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRRootTemplateSetNetwork(IRContext context, int hTemplate, uint64_t networkID);
+
+/**
+* Disposes a root record template.
+*
+* @param[in] context The new context.
+* @param[out] hTemplate The disposed root record template.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRRootTemplateDispose(IRContext context, int * hTemplate);
+
+/** @}*/ //addtogroup irecord_pub_root_record_template
+
+//==============================================================================
+// State Handling
+//------------------------------------------------------------------------------
+/**
+* @defgroup irecord_pub_state_handling State Handling
+*/
+/** @addtogroup irecord_pub_state_handling
+*  @{
+*/
+
+/**
+* Creates a new state.
+*
+* @param[in] context The new context.
+* @param[out] hState The state created.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRInstanceStateCreate(IRContext context, int ** hState);
+
+/**
+* Sets a new state parameter.
+*
+* @param[in] context The new context.
+* @param[in] hState The state.
+* @param[in] param The state parameter to be set.
+* @param[in] value The state parameter value to be set
+* @note  Parameters such as Hash type, Signature algorithm type among others
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRInstanceStateSetParam(IRContext context, int * hState, int param, int value);
+
+/**
+* Loads a state.
+*
+* @param[in] context The new context.
+* @param[in] stateSize The size of the state.
+* @param[in] state The bytes of the state.
+* @param[out] hState The state loaded.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRInstanceStateLoad(IRContext context, int stateSize, const void * state, int ** hState);
+
+/**
+* Serializes a state.
+*
+* @param[in] context The new context.
+* @param[in] hState The state.
+* @param[out] buffSize The buffer size of the serialized state.
+* @param[out] buff The serialized state buffer.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRInstanceStateSerialize(IRContext context, int hState, int ** buffSize, void * buff);
+
+/**
+* Diosposes a state.
+*
+* @param[in] context The new context.
+* @param[in] hState The state to be disposed.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRInstanceStateDispose(IRContext context, int hState);
+
+/** @}*/ //addtogroup irecord_pub_state_handling
+
+//==============================================================================
+// Block Handling
+//------------------------------------------------------------------------------
+/**
+* @defgroup irecord_pub_block_handling Block Handling
+*/
+/** @addtogroup irecord_pub_block_handling
+*  @{
+*/
+
+/**
+* Loads a block.
+*
+* @param[in] context The new context.
+* @param[in] blockSize The block size to be loaded.
+* @param[in] block The bytes of the block.
+* @param[out] hBlock The block loaded.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRBlockLoad(IRContext context, int blockSize, const void * block, int * hBlock);
+
+/**
+* Serializes a block.
+*
+* @param[in] context The new context.
+* @param[in] hBlock The block.
+* @param[out] buffSize The buffer size of the serialized block.
+* @param[out] buff The serialized block buffer.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRBlockSerialize(IRContext context, int hBlock, int ** buffSize, void * buff);
+
+/**
+* Diosposes a block.
+*
+* @param[in] context The new context.
+* @param[in] hBlock The block to be disposed.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRBlockDispose(IRContext context, int hBlock);
+
+/** @}*/ //addtogroup irecord_pub_block_handling
+
+//==============================================================================
+// Root block
+//------------------------------------------------------------------------------
+/**
+* @defgroup irecord_pub_root_block Root block
+*/
+/** @addtogroup irecord_pub_root_block
+*  @{
+*/
+
+/**
+* Creates a root block.
+*
+* @param[in] context The new context.
+* @param[in] hTemplate The root record template.
+* @param[out] hState The root block state.
+* @param[out] hBlock The block loaded.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRRootBlockCreate(IRContext context, int hTemplate, int * hState, int * hBlock);
+
+/** @}*/ //addtogroup irecord_pub_root_block
+
+//==============================================================================
+// Data record
+//------------------------------------------------------------------------------
+/**
+* @defgroup irecord_pub_data_record Data record
+*/
+/** @addtogroup irecord_pub_data_record
+*  @{
+*/
+
+/**
+* Adds a data block.
+*
+* @param[in] context The new context.
+* @param[in] hState The root block state.
+* @param[in] lockKey The lock key.
+* @param[in] hParentBlock The parent block.
+* @param[in] applicationId The ID of the Application.
+* @param[in] payloadSize The payload size.
+* @param[in] payload The payload of the data block.
+* @param[out] hBlock The data block loaded.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRDataBlockAdd(IRContext context, int hState, int lockKey, int hParentBlock,
+	uint64_t applicationId, int payloadSize, int payload, int * hBlock);
+
+
+/** @}*/ //addtogroup irecord_pub_data_record
+
+
+//==============================================================================
+// Closing Record
+//------------------------------------------------------------------------------
+/**
+* @defgroup irecord_pub_closing_record Closing record
+*/
+/** @addtogroup irecord_pub_closing_record
+*  @{
+*/
+
+/**
+* Closes a record.
+*
+* @param[in] context The new context.
+* @param[in] hState The block state.
+* @param[in] hParentBlock The parent block.
+* @param[in] reason The reason of closing this block.
+* @param[in] comments Comments about closing this block.
+* @param[in] hSucessor The sucessor block.
+* @param[out] hBlock The block closed.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRClose(IRContext context, int hState, int hParentBlock, int reason, const char * comments, int hSucessor, int * hBlock);
+
+/**
+* Emergency close.
+*
+* @param[in] context The new context.
+* @param[in] hState The block state.
+* @param[in] hParentBlock The parent block.
+* @param[in] hEmerngencyKey The emergency key.
+* @param[in] reason The reason of closing this block.
+* @param[in] comments Comments about closing this block.
+* @param[out] hBlock The block emergency closed.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IREmergencyClose(IRContext context, int hState, int hParentBlock, int hEmerngencyKey, int reason, const char * comments, int * hBlock);
+
+/** @}*/ //addtogroup irecord_pub_closing_record
+
+//==============================================================================
+// Verification
+//------------------------------------------------------------------------------
+/**
+* @defgroup irecord_pub_verification Verification
+*/
+/** @addtogroup irecord_pub_verification
+*  @{
+*/
+
+/**
+* Checks the root block.
+*
+* @param[in] context The new context.
+* @param[in] hRootBlock The root block.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRCheckRoot(IRContext context, int hRootBlock);
+
+/**
+* Checks the parent block.
+*
+* @param[in] context The new context.
+* @param[in] hBlock The actual block.
+* @param[in] hParentBlock The parent block.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRCheckParent(IRContext context, int hBlock, int hParentBlock);
+
+/**
+* Checks the emergency closing block.
+*
+* @param[in] context The new context.
+* @param[in] hBlock The actual block.
+* @param[in] hRootBlock The root block.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRCheckEmergencyClosing(IRContext context, int hBlock, int hRootBlock);
+
+/**
+* Block Parameters
+*
+* @param[in] context The new context.
+* @param[in] hBlock The actual block.
+* @param[in] param The parameter.
+* @param[in] value The parameter value.
+* @return IRE_SUCCESS on success or other error code in case of failure.
+*/
+IR_EXPORT_ATTR int IR_EXPORT_CALL IRBlockParameter(IRContext context, int hBlock, int param, uint64_t * value);
+
+/** @}*/ //addtogroup irecord_pub_verification
 
 /** @}*/ //addtogroup irecord_pub
 #endif // __irecord_irecord_H__
