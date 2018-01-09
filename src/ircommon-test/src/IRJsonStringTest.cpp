d55cf199694f18dd5de426206ae2ea95ca8246af
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-01-09 03:01:05 -0200

    Unit-tests of IRJsonString completed.

--- src/ircommon-test/src/IRJsonStringTest.cpp
+++ src/ircommon-test/src/IRJsonStringTest.cpp
@@ -25,6 +25,8 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "IRJsonStringTest.h"
+#include <ircommon/irjson.h>
+using namespace ircommon::json;
 
 //==============================================================================
 // class IRJsonStringTest
@@ -46,9 +48,64 @@ void IRJsonStringTest::TearDown() {
 
 //------------------------------------------------------------------------------
 TEST_F(IRJsonStringTest,Constructor) {
+	IRJsonString * s;
 
-	//TODO Implementation required!
-	std::cout << "Implementation required!";
+	s = new IRJsonString();
+	ASSERT_EQ(IRJsonValue::STRING, s->type());
+	ASSERT_STREQ("", s->get().c_str());
+	delete s;
+
+	s = new IRJsonString("value");
+	ASSERT_EQ(IRJsonValue::STRING, s->type());
+	ASSERT_STREQ("value", s->get().c_str());
+	delete s;
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonStringTest,getSet) {
+	IRJsonString s("value");
+
+	ASSERT_STREQ("value", s.get().c_str());
+	s.set("21");
+	ASSERT_STREQ("21", s.get().c_str());
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonStringTest, as) {
+	IRJsonString s("value");
+
+	try{
+		s.asBoolean();
+		FAIL();
+	} catch(std::domain_error & e){}
+	try{
+		s.asDecimal();
+		FAIL();
+	} catch(std::domain_error & e){}
+	try{
+		s.asInteger();
+		FAIL();
+	} catch(std::domain_error & e){}
+	ASSERT_STREQ("value", s.asString().c_str());
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonStringTest, equals) {
+	IRJsonString a("value");
+	IRJsonString b("value");
+	IRJsonString c("value2");
+
+	ASSERT_TRUE(a.equals(a));
+	ASSERT_TRUE(a.equals(b));
+	ASSERT_FALSE(a.equals(c));
+
+	ASSERT_FALSE(a.equals(IRJsonArray()));
+	ASSERT_FALSE(a.equals(IRJsonBoolean()));
+	ASSERT_FALSE(a.equals(IRJsonDecimal()));
+	ASSERT_FALSE(a.equals(IRJsonInteger()));
+	ASSERT_FALSE(a.equals(IRJsonNull()));
+	ASSERT_FALSE(a.equals(IRJsonObject()));
+	ASSERT_FALSE(a.equals(IRJsonString()));
 }
 //------------------------------------------------------------------------------
 
