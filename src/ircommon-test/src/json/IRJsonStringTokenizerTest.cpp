0bf7ed3c01359fb161605c7ef2bd36a664a30e6a
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-01-15 04:03:40 -0200

    Implementation of the unit test of the IRJsonStringTokenizer completed.

--- src/ircommon-test/src/json/IRJsonStringTokenizerTest.cpp
+++ src/ircommon-test/src/json/IRJsonStringTokenizerTest.cpp
@@ -25,6 +25,8 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "IRJsonStringTokenizerTest.h"
+#include <ircommon/irjson.h>
+using namespace ircommon::json;
 
 //==============================================================================
 // class IRJsonStringTokenizerTest
@@ -46,9 +48,66 @@ void IRJsonStringTokenizerTest::TearDown() {
 
 //------------------------------------------------------------------------------
 TEST_F(IRJsonStringTokenizerTest,Constructor) {
+	IRJsonStringTokenizer * t;
+	std::string s = "12345";
 
-	//TODO Implementation required!
-	std::cout << "Implementation required!";
+	t = new IRJsonStringTokenizer(s);
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t->next());
+	ASSERT_STREQ(s.c_str(), t->value().c_str());
+	delete t;
+
+	t = new IRJsonStringTokenizer(s, 1, 3);
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t->next());
+	ASSERT_STREQ("234", t->value().c_str());
+	delete t;
+
+	t = new IRJsonStringTokenizer(s.c_str());
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t->next());
+	ASSERT_STREQ(s.c_str(), t->value().c_str());
+	delete t;
+
+	t = new IRJsonStringTokenizer(s.c_str(), 3);
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t->next());
+	ASSERT_STREQ("123", t->value().c_str());
+	delete t;
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonStringTokenizerTest,reset) {
+	IRJsonStringTokenizer t("12345");
+
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t.next());
+	ASSERT_EQ(IRJsonTokenizer::INPUT_END, t.next());
+	ASSERT_FALSE(t.hasMore());
+	ASSERT_TRUE(t.reset());
+	ASSERT_TRUE(t.hasMore());
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT , t.next());
+	ASSERT_EQ(IRJsonTokenizer::INPUT_END, t.next());
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonStringTokenizerTest,hasMore) {
+	IRJsonStringTokenizer t("12345 12345");
+
+	ASSERT_TRUE(t.hasMore());
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t.next());
+	ASSERT_TRUE(t.hasMore());
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t.next());
+	ASSERT_FALSE(t.hasMore());
+	ASSERT_EQ(IRJsonTokenizer::INPUT_END, t.next());
+	ASSERT_FALSE(t.hasMore());
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonStringTokenizerTest, position) {
+	IRJsonStringTokenizer t("12345 12345");
+
+	ASSERT_EQ(0, t.position());
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t.next());
+	ASSERT_EQ(5, t.position());
+	ASSERT_EQ(IRJsonTokenizer::VAL_INT, t.next());
+	ASSERT_EQ(11, t.position());
 }
 //------------------------------------------------------------------------------
+
 
