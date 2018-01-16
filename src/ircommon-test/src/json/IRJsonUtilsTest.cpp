aff772dde4dd18459abb270c5bf5c3c0543b0dc4
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-01-16 00:18:53 -0200

    Unit tests for utility functions completed.

--- src/ircommon-test/src/json/IRJsonUtilsTest.cpp
+++ src/ircommon-test/src/json/IRJsonUtilsTest.cpp
@@ -25,6 +25,8 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "IRJsonUtilsTest.h"
+#include <ircommon/irjson.h>
+using namespace ircommon::json;
 
 //==============================================================================
 // class IRJsonUtilsTest
@@ -45,10 +47,276 @@ void IRJsonUtilsTest::TearDown() {
 }
 
 //------------------------------------------------------------------------------
-TEST_F(IRJsonUtilsTest,Constructor) {
+TEST_F(IRJsonUtilsTest, IRJsonAsArray) {
+	IRJsonArray arrayObj;
+	IRJsonBoolean boolObj;
+	IRJsonDecimal decObj;
+	IRJsonInteger intObj;
+	IRJsonNull nullObj;
+	IRJsonObject objObj;
+	IRJsonString strObj;
 
-	//TODO Implementation required!
-	std::cout << "Implementation required!";
+	try {
+		IRJsonAsArray(arrayObj);
+	}catch (std::invalid_argument &e){FAIL();}
+	try {
+		IRJsonAsArray(boolObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsArray(decObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsArray(intObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsArray(nullObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsArray(objObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsArray(strObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonUtilsTest, IRJsonAsBoolean) {
+	IRJsonArray arrayObj;
+	IRJsonBoolean boolObj;
+	IRJsonDecimal decObj;
+	IRJsonInteger intObj;
+	IRJsonNull nullObj;
+	IRJsonObject objObj;
+	IRJsonString strObj;
+
+	try {
+		IRJsonAsBoolean(arrayObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsBoolean(boolObj);
+	}catch (std::invalid_argument &e){FAIL();}
+	try {
+		IRJsonAsBoolean(decObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsBoolean(intObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsBoolean(nullObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsBoolean(objObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsBoolean(strObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonUtilsTest, IRJsonAsDecimal) {
+	IRJsonArray arrayObj;
+	IRJsonBoolean boolObj;
+	IRJsonDecimal decObj;
+	IRJsonInteger intObj;
+	IRJsonNull nullObj;
+	IRJsonObject objObj;
+	IRJsonString strObj;
+
+	try {
+		IRJsonAsDecimal(arrayObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsDecimal(boolObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsDecimal(decObj);
+	}catch (std::invalid_argument &e){FAIL();}
+	try {
+		IRJsonAsDecimal(intObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsDecimal(nullObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsDecimal(objObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsDecimal(strObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonUtilsTest, IRJsonAsInteger) {
+	IRJsonArray arrayObj;
+	IRJsonBoolean boolObj;
+	IRJsonDecimal decObj;
+	IRJsonInteger intObj;
+	IRJsonNull nullObj;
+	IRJsonObject objObj;
+	IRJsonString strObj;
+
+	try {
+		IRJsonAsInteger(arrayObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsInteger(boolObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsInteger(decObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsInteger(intObj);
+	}catch (std::invalid_argument &e){FAIL();}
+	try {
+		IRJsonAsInteger(nullObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsInteger(objObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsInteger(strObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonUtilsTest, IRJsonAsNull) {
+	IRJsonArray arrayObj;
+	IRJsonBoolean boolObj;
+	IRJsonDecimal decObj;
+	IRJsonInteger intObj;
+	IRJsonNull nullObj;
+	IRJsonObject objObj;
+	IRJsonString strObj;
+
+	try {
+		IRJsonAsNull(arrayObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsNull(boolObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsNull(decObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsNull(intObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsNull(nullObj);
+	}catch (std::invalid_argument &e){FAIL();}
+	try {
+		IRJsonAsNull(objObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsNull(strObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonUtilsTest, IRJsonAsObject) {
+	IRJsonArray arrayObj;
+	IRJsonBoolean boolObj;
+	IRJsonDecimal decObj;
+	IRJsonInteger intObj;
+	IRJsonNull nullObj;
+	IRJsonObject objObj;
+	IRJsonString strObj;
+
+	try {
+		IRJsonAsObject(arrayObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsObject(boolObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsObject(decObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsObject(intObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsObject(nullObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsObject(objObj);
+	}catch (std::invalid_argument &e){FAIL();}
+	try {
+		IRJsonAsObject(strObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRJsonUtilsTest, IRJsonAsString) {
+	IRJsonArray arrayObj;
+	IRJsonBoolean boolObj;
+	IRJsonDecimal decObj;
+	IRJsonInteger intObj;
+	IRJsonNull nullObj;
+	IRJsonObject objObj;
+	IRJsonString strObj;
+
+	try {
+		IRJsonAsString(arrayObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsString(boolObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsString(decObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsString(intObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsString(nullObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsString(objObj);
+		FAIL();
+	}catch (std::invalid_argument &e){}
+	try {
+		IRJsonAsString(strObj);
+	}catch (std::invalid_argument &e){FAIL();}
 }
 //------------------------------------------------------------------------------
 
