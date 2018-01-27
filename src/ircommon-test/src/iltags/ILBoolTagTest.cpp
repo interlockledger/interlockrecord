2dfeaa3c185159f8b3fb3a1b25b810e5b5daa2c4
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-01-27 03:24:29 -0200

    Unit tests for ILBoolTag completed.

--- src/ircommon-test/src/iltags/ILBoolTagTest.cpp
+++ src/ircommon-test/src/iltags/ILBoolTagTest.cpp
@@ -25,6 +25,9 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "ILBoolTagTest.h"
+#include <ircommon/iltagstd.h>
+using namespace ircommon;
+using namespace ircommon::iltags;
 
 //==============================================================================
 // class ILBoolTagTest
@@ -46,9 +49,59 @@ void ILBoolTagTest::TearDown() {
 
 //------------------------------------------------------------------------------
 TEST_F(ILBoolTagTest,Constructor) {
+	ILBoolTag * t;
 
-	//TODO Implementation required!
-	std::cout << "Implementation required!";
+	t = new ILBoolTag();
+	ASSERT_EQ(ILTag::TAG_BOOL, t->id());
+	ASSERT_FALSE(t->value());
+	delete t;
 }
+
+//------------------------------------------------------------------------------
+TEST_F(ILBoolTagTest, size) {
+	ILBoolTag t;
+
+	ASSERT_EQ(1, t.size());
+}
+
+//------------------------------------------------------------------------------
+TEST_F(ILBoolTagTest, serialize) {
+	ILBoolTag t;
+	IRBuffer out;
+
+	ASSERT_TRUE(t.serialize(out));
+	ASSERT_EQ(2, out.size());
+	ASSERT_EQ(ILTag::TAG_BOOL, out.roBuffer()[0]);
+	ASSERT_EQ(0, out.roBuffer()[1]);
+
+	out.setSize(0);
+	t.setValue(true);
+	ASSERT_TRUE(t.serialize(out));
+	ASSERT_EQ(2, out.size());
+	ASSERT_EQ(ILTag::TAG_BOOL, out.roBuffer()[0]);
+	ASSERT_EQ(1, out.roBuffer()[1]);
+}
+
+//------------------------------------------------------------------------------
+TEST_F(ILBoolTagTest, deserializeValue) {
+	ILBoolTag t;
+	std::uint8_t serialized[8];
+	ILTagFactory f;
+
+	std::memset(serialized, 0, sizeof(serialized));
+	t.setValue(true);
+	ASSERT_TRUE(t.deserializeValue(f, serialized, 1));
+	ASSERT_FALSE(t.value());
+
+	std::memset(serialized, 0, sizeof(serialized));
+	serialized[0] = 1;
+	t.setValue(false);
+	ASSERT_TRUE(t.deserializeValue(f, serialized, 1));
+	ASSERT_TRUE(t.value());
+
+	ASSERT_FALSE(t.deserializeValue(f, serialized, 0));
+	ASSERT_FALSE(t.deserializeValue(f, serialized, 2));
+}
+
 //------------------------------------------------------------------------------
 
