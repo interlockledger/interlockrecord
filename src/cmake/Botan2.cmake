5785602e034cb81297cc21f953596de8b30476ff
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-02-06 00:39:57 -0200

    Build script adjusted to the new version of the modules MSVCCRT.cmake and Botan2.cmake.

--- src/cmake/Botan2.cmake
+++ src/cmake/Botan2.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2017, Open Communications Security
+# Copyright (c) 2017-2018, Open Communications Security
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -23,17 +23,29 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# This module Locates the Botan2 libraries and headers. It defines the following
-# constants:
+# This module locates Botan2 and defines the external library Botan2 on success.
+#
+# It also defines the following constants:
 #    - BOTAN2_LIB: The Botan 2 library;
 #    - BOTAN2D_LIB: The Botan 2 library for debug;
 #    - BOTAN2_INCLUDE_DIR: The Botan 2 include library;
-#    - BOTAN2_DEP_LIBS: The other libraries require by Botan 2;
 #    - BOTAN2_VERSION: The version of the Botan library extracted from "build.h";
 #    - BOTAN2_FOUND: Flag that indicates that the Botan2 library was found;
 #
-# Knwon limitations:
-# This version of the module was not tested on MacOS.
+# This method depends on MSVCCRT from OpenCS in order to work properly.
+#
+# Known limitations:
+# 	- This version of the module was not tested on MacOS.
+if(__OPENCS_BOTAN2)
+  return()
+endif()
+set(__OPENCS_BOTAN2 1)
+
+# Check the dependency on 
+if (NOT __OPENCS_MSVCCRT)
+	message(FATAL_ERROR "OpenCS Botan2 module requires OpenCS MSVCCRT module in order to work properly.")
+endif()
+
 if (WIN32)
 	if(CMAKE_CL_64)
 		set(platform "x64")
@@ -63,10 +75,9 @@ if (WIN32)
 		PATH_SUFFIXES 
 			"include/botan-2"
 			"botan-2")
-	set(BOTAN2_DEP_LIBS "Ws2_32.lib")
-	set(BOTAN2D_LIB "")
-	set(BOTAN2_DEP_LIBS "")
+	set(BOTAN2_DEPS Ws2_32.lib)
 else()
+	# Linux and other
 	find_library(BOTAN2_LIB
 		"libbotan-2.a")
 	find_library(BOTAN2_SHARED_LIB
@@ -75,8 +86,7 @@ else()
 		"botan/botan.h"
 		PATH_SUFFIXES "botan-2")
 	# TODO Fix this dependency later
-	set(BOTAN2D_LIB "")
-	set(BOTAN2_DEP_LIBS "")
+	set(BOTAN2D_LIB "${BOTAN2_LIB}")
 endif()
 
 # Extract the Botan version from build.h
@@ -93,6 +103,27 @@ if(DEFINED BOTAN2_LIB)
 	if(DEFINED BOTAN2_INCLUDE_DIR)
 		set(BOTAN2_FOUND 1)
 		message(STATUS "Botan ${BOTAN2_VERSION} found.")
+		
+		# Define the library
+		if(NOT TARGET Botan2)
+			add_library(Botan2 UNKNOWN IMPORTED)
+			set_target_properties(Botan2 PROPERTIES
+				INTERFACE_INCLUDE_DIRECTORIES "${BOTAN2_INCLUDE_DIR}")
+			if(DEFINED BOTAN2_DEPS)
+				set_target_properties(Botan2 PROPERTIES
+					INTERFACE_LINK_LIBRARIES "${BOTAN2_DEPS}")
+			endif()
+            set_property(TARGET Botan2 APPEND PROPERTY
+				IMPORTED_CONFIGURATIONS RELEASE)
+            set_target_properties(Botan2 PROPERTIES
+				IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+				IMPORTED_LOCATION_RELEASE "${BOTAN2_LIB}")
+            set_property(TARGET Botan2 APPEND PROPERTY
+                IMPORTED_CONFIGURATIONS DEBUG)
+            set_target_properties(Botan2 PROPERTIES
+                IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+                IMPORTED_LOCATION_DEBUG "${BOTAN2D_LIB}")
+		endif()		
 	endif()
 endif()
 
