4f8fb43492498b15395f27d20c52feb091b37022
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-03-07 14:56:16 -0300

    Unit test for IRHash completed.

--- src/irecordcore-test/src/crypto/IRHashTest.cpp
+++ src/irecordcore-test/src/crypto/IRHashTest.cpp
@@ -25,6 +25,52 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "IRHashTest.h"
+#include "IRBotanHashTest.h"
+#include <irecordcore/irhash.h>
+
+using namespace irecordcore;
+using namespace irecordcore::crypto;
+
+//==============================================================================
+// class DummyIRHash
+//------------------------------------------------------------------------------
+class DummyIRHash: public IRHash {
+private:
+	 std::uint64_t _size;
+public:
+	DummyIRHash(IRHashAlg type):IRHash(type), _size(0){}
+	virtual ~DummyIRHash() = default;
+
+	void setSize( std::uint64_t size) {
+		this->_size = size;
+	}
+
+	virtual void reset();
+
+	virtual std::uint64_t size() const;
+
+	virtual void update(const void * buff, std::uint64_t size);
+
+	virtual bool finalize(void * out, std::uint64_t size);
+};
+
+//------------------------------------------------------------------------------
+void DummyIRHash::reset() {
+}
+
+//------------------------------------------------------------------------------
+std::uint64_t DummyIRHash::size() const {
+	return this->_size;
+}
+
+//------------------------------------------------------------------------------
+void DummyIRHash::update(const void * buff, std::uint64_t size) {
+}
+
+//------------------------------------------------------------------------------
+bool DummyIRHash::finalize(void * out, std::uint64_t size) {
+	return false;
+}
 
 //==============================================================================
 // class IRHashTest
@@ -45,10 +91,49 @@ void IRHashTest::TearDown() {
 }
 
 //------------------------------------------------------------------------------
-TEST_F(IRHashTest,Constructor) {
+TEST_F(IRHashTest, Constructor) {
+	IRHash * h;
 
-	//TODO Implementation required!
-	std::cout << "Implementation required!";
+	h = new DummyIRHash(IR_HASH_SHA1);
+	ASSERT_EQ(IR_HASH_SHA1, h->type());
+	ASSERT_EQ(0, h->size());
+	delete h;
+
+	h = new DummyIRHash(IR_HASH_SHA256);
+	ASSERT_EQ(IR_HASH_SHA256, h->type());
+	ASSERT_EQ(0, h->size());
+	delete h;
 }
+
+//------------------------------------------------------------------------------
+TEST_F(IRHashTest, type) {
+	IRHashAlg types[6] = {
+		IR_HASH_SHA1,
+		IR_HASH_SHA256,
+		IR_HASH_SHA512,
+		IR_HASH_SHA3_256,
+		IR_HASH_SHA3_512,
+		IR_HASH_COPY};
+
+	for (int i = 0; i < 6; i++) {
+		DummyIRHash h(types[i]);
+		ASSERT_EQ(types[i], h.type());
+	}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRHashTest, sizeInBytes) {
+	DummyIRHash h(IR_HASH_SHA1);
+
+	h.setSize(0);
+	ASSERT_EQ(0, h.sizeInBytes());
+
+	h.setSize(8);
+	ASSERT_EQ(1, h.sizeInBytes());
+
+	h.setSize(32);
+	ASSERT_EQ(4, h.sizeInBytes());
+}
+
 //------------------------------------------------------------------------------
 
