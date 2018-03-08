7ddb45414f14e2da013dc85101340d9e0a926daa
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-03-08 13:24:34 -0300

    Unit test for IRSHA256Hash completed.

--- src/irecordcore-test/src/crypto/IRSHA256HashTest.cpp
+++ src/irecordcore-test/src/crypto/IRSHA256HashTest.cpp
@@ -25,6 +25,11 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "IRSHA256HashTest.h"
+#include <irecordcore/irhash.h>
+#include "CryptoSamples.h"
+
+using namespace irecordcore;
+using namespace irecordcore::crypto;
 
 //==============================================================================
 // class IRSHA256HashTest
@@ -46,9 +51,53 @@ void IRSHA256HashTest::TearDown() {
 
 //------------------------------------------------------------------------------
 TEST_F(IRSHA256HashTest,Constructor) {
+	IRSHA256Hash * h;
 
-	//TODO Implementation required!
-	std::cout << "Implementation required!";
+	h = new IRSHA256Hash();
+	ASSERT_EQ(IR_HASH_SHA256, h->type());
+	delete h;
 }
+
+//------------------------------------------------------------------------------
+TEST_F(IRSHA256HashTest, size) {
+	IRSHA256Hash h;
+
+	ASSERT_EQ(256, h.size());
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRSHA256HashTest, update) {
+	IRSHA256Hash h;
+	std::uint8_t out[32];
+	const std::uint8_t * p;
+	const std::uint8_t * pEnd;
+
+	p = CRYPTOSAMPLES_SAMPLE;
+	pEnd = p + sizeof(CRYPTOSAMPLES_SAMPLE);
+
+	while (p < pEnd) {
+		h.update(p, 2);
+		p += 2;
+	}
+	ASSERT_TRUE(h.finalize(out, sizeof(out)));
+	ASSERT_EQ(0, memcmp(CRYPTOSAMPLES_SHA256_SAMPLE, out,
+			sizeof(CRYPTOSAMPLES_SHA256_SAMPLE)));
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRSHA256HashTest, finalize) {
+	IRSHA256Hash h;
+	std::uint8_t exp[32];
+	std::uint8_t out[sizeof(exp)];
+
+	ASSERT_TRUE(h.finalize(exp, sizeof(exp)));
+	ASSERT_EQ(0, memcmp(exp, CRYPTOSAMPLES_SHA256_EMPTY, sizeof(exp)));
+
+	h.update(CRYPTOSAMPLES_SAMPLE, sizeof(CRYPTOSAMPLES_SAMPLE));
+	ASSERT_TRUE(h.finalize(out, sizeof(out)));
+	ASSERT_EQ(0, memcmp(CRYPTOSAMPLES_SHA256_SAMPLE, out,
+			sizeof(CRYPTOSAMPLES_SHA256_SAMPLE)));
+}
+
 //------------------------------------------------------------------------------
 
