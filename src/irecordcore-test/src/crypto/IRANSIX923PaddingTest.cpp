8c40d7e03e96ff6dc06ca3fc1d5ad3f81b18dc03
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-04-05 16:36:20 -0300

    Implementation of IRANSIX923Padding finished alongside its tests.

--- src/irecordcore-test/src/crypto/IRANSIX923PaddingTest.cpp
+++ src/irecordcore-test/src/crypto/IRANSIX923PaddingTest.cpp
@@ -25,7 +25,10 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "IRANSIX923PaddingTest.h"
+#include <irecordcore/irciphpd.h>
+#include <cstring>
 
+using namespace irecordcore::crypto;
 //==============================================================================
 // class IRANSIX923PaddingTest
 //------------------------------------------------------------------------------
@@ -46,9 +49,97 @@ void IRANSIX923PaddingTest::TearDown() {
 
 //------------------------------------------------------------------------------
 TEST_F(IRANSIX923PaddingTest,Constructor) {
+	IRANSIX923Padding * p;
 
-	//TODO Implementation required!
-	std::cout << "Implementation required!";
+	p = new IRANSIX923Padding();
+	delete p;
 }
-//------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
+TEST_F(IRANSIX923PaddingTest, addPadding) {
+	IRANSIX923Padding p;
+	std::uint8_t src[33];
+	std::uint8_t dst[33];
+	std::uint8_t exp[33];
+	std::uint64_t srcSize;
+	std::uint64_t dstSize;
+	unsigned int paddingSize;
+
+	for (srcSize = 0; srcSize <= 16; srcSize++) {
+		std::memset(src, 1, sizeof(src));
+		std::memset(dst, 2, sizeof(dst));
+
+		paddingSize = 16 - (srcSize % 16);
+		dstSize = sizeof(dst);
+		ASSERT_TRUE(p.addPadding(16, src, srcSize, dst, dstSize));
+		ASSERT_EQ(srcSize + paddingSize, dstSize);
+		ASSERT_EQ(2, dst[dstSize]);
+
+		std::memset(exp, 1, srcSize);
+		std::memset(exp + srcSize, 0, paddingSize - 1);
+		exp[dstSize - 1] = paddingSize;
+		ASSERT_EQ(0, std::memcmp(exp, dst, dstSize));
+	}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IRANSIX923PaddingTest, removePadding) {
+	IRANSIX923Padding p;
+	std::uint8_t src[32];
+	std::uint64_t srcSize;
+
+	std::memset(src, 0, sizeof(src));
+	srcSize = 0;
+	ASSERT_TRUE(p.removePadding(32, src, srcSize));
+	ASSERT_EQ(0, srcSize);
+
+	for (unsigned int paddingSize = 1; paddingSize <= 16; paddingSize++) {
+		srcSize = 16 - paddingSize;
+		std::memset(src, 0, sizeof(src));
+		std::memset(src + srcSize, 0, paddingSize - 1);
+		src[srcSize+ paddingSize - 1] = paddingSize;
+
+		srcSize = 16;
+		ASSERT_TRUE(p.removePadding(16, src, srcSize));
+		ASSERT_EQ(16 - paddingSize, srcSize);
+	}
+
+	for (unsigned int paddingSize = 1; paddingSize <= 16; paddingSize++) {
+		srcSize = 16 - paddingSize;
+		std::memset(src, 0, sizeof(src));
+		std::memset(src + srcSize + 16, 0, paddingSize - 1);
+		src[srcSize + 16 + paddingSize - 1] = paddingSize;
+
+		srcSize = sizeof(src);
+		ASSERT_TRUE(p.removePadding(16, src, srcSize));
+		ASSERT_EQ(sizeof(src) - paddingSize, srcSize);
+	}
+
+	// Broken padding
+	for (unsigned int paddingSize = 2; paddingSize <= 16; paddingSize++) {
+		srcSize = 16 - paddingSize;
+		std::memset(src, 0, sizeof(src));
+		std::memset(src + srcSize, 0, paddingSize - 1);
+		src[srcSize+ paddingSize - 1] = paddingSize;
+
+		std::uint8_t tmp2[32];
+		for (unsigned int i = 1; i < paddingSize; i++) {
+			std::memcpy(tmp2, src, sizeof(tmp2));
+			tmp2[15 - i]++;
+			srcSize = 16;
+			ASSERT_FALSE(p.removePadding(16, tmp2, srcSize));
+			ASSERT_EQ(16, srcSize);
+		}
+	}
+
+	// Invalid padding size
+	for (unsigned int paddingSize = 17; paddingSize < 256; paddingSize++) {
+		std::memset(src, paddingSize, sizeof(src));
+
+		srcSize = 16;
+		ASSERT_FALSE(p.removePadding(16, src, srcSize));
+			ASSERT_EQ(16, srcSize);
+	}
+}
+
+//------------------------------------------------------------------------------
