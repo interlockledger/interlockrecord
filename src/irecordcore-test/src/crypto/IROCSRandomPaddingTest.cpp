b70f47ef8902403e924acc4e6c31e84ae12c178e
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-04-06 16:10:38 -0300

    Implementation of IROCSRandomPadding and its tests completed.

--- src/irecordcore-test/src/crypto/IROCSRandomPaddingTest.cpp
+++ src/irecordcore-test/src/crypto/IROCSRandomPaddingTest.cpp
@@ -25,6 +25,12 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 #include "IROCSRandomPaddingTest.h"
+#include <irecordcore/irciphpd.h>
+#include <ircommon/irrandom.h>
+#include <cstring>
+
+using namespace ircommon;
+using namespace irecordcore::crypto;
 
 //==============================================================================
 // class IROCSRandomPaddingTest
@@ -46,9 +52,81 @@ void IROCSRandomPaddingTest::TearDown() {
 
 //------------------------------------------------------------------------------
 TEST_F(IROCSRandomPaddingTest,Constructor) {
+	IROCSRandomPadding * p;
 
-	//TODO Implementation required!
-	std::cout << "Implementation required!";
+	p = new IROCSRandomPadding(new IRXORShifRandom());
+	delete p;
 }
+
+//------------------------------------------------------------------------------
+TEST_F(IROCSRandomPaddingTest, addPadding) {
+	IROCSRandomPadding p(new IRXORShifRandom(2258));
+	IRXORShifRandom r(2258);
+	std::uint8_t src[33];
+	std::uint8_t dst[33];
+	std::uint8_t exp[33];
+	std::uint64_t srcSize;
+	std::uint64_t dstSize;
+	unsigned int paddingSize;
+
+	for (srcSize = 0; srcSize <= 16; srcSize++) {
+		std::memset(src, 1, sizeof(src));
+		std::memset(dst, 2, sizeof(dst));
+
+		paddingSize = 16 - (srcSize % 16);
+		dstSize = sizeof(dst);
+		ASSERT_TRUE(p.addPadding(16, src, srcSize, dst, dstSize));
+		ASSERT_EQ(srcSize + paddingSize, dstSize);
+		ASSERT_EQ(2, dst[dstSize]);
+
+		std::memset(exp, 1, srcSize);
+		r.nextBytes(exp + srcSize, paddingSize);
+		exp[srcSize + paddingSize - 1] =
+				exp[srcSize + paddingSize - 1] -
+				(exp[srcSize + paddingSize - 1] % 16) + paddingSize;
+		ASSERT_EQ(0, std::memcmp(exp, dst, dstSize));
+	}
+}
+
+//------------------------------------------------------------------------------
+TEST_F(IROCSRandomPaddingTest, removePadding) {
+	IROCSRandomPadding p(new IRXORShifRandom(2258));
+	IRXORShifRandom r(2258);
+	std::uint8_t src[32];
+	std::uint64_t srcSize;
+	unsigned int paddingSize;
+
+	std::memset(src, 0, sizeof(src));
+	srcSize = 0;
+	ASSERT_TRUE(p.removePadding(32, src, srcSize));
+	ASSERT_EQ(0, srcSize);
+
+	for (unsigned int last = 0; last < 256; last++) {
+		r.nextBytes(src, sizeof(src));
+		src[15] = last;
+		paddingSize = last % 16;
+		if (paddingSize == 0) {
+			paddingSize = 16;
+		}
+
+		srcSize = 16;
+		ASSERT_TRUE(p.removePadding(16, src, srcSize));
+		ASSERT_EQ(16 - paddingSize, srcSize);
+	}
+
+	for (unsigned int last = 0; last < 256; last++) {
+		r.nextBytes(src, sizeof(src));
+		src[31] = last;
+		paddingSize = last % 16;
+		if (paddingSize == 0) {
+			paddingSize = 16;
+		}
+
+		srcSize = 32;
+		ASSERT_TRUE(p.removePadding(16, src, srcSize));
+		ASSERT_EQ(32 - paddingSize, srcSize);
+	}
+}
+
 //------------------------------------------------------------------------------
 
