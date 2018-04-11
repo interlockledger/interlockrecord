b43c14284b75e774f61b52fb166a285d5b3c1f34
Fabio Jun Takada Chino <fchino@opencs.com.br>
2018-04-11 15:48:26 -0300

    Removing useless files. Now the script will generate it all.

--- tools/samples/aes/generate.sh
+++ tools/samples/aes/generate.sh
@@ -2,6 +2,11 @@
 
 # This program generates the samples for testing with AES using OpenSSL
 
+# Simple... logic.
+CRYPTOSAMPLES_KEY128=53696d706c652e2e2e206c6f6769632e
+# Kosh: You have always been here.
+CRYPTOSAMPLES_KEY256=4b6f73683a20596f75206861766520616c77617973206265656e20686572652e
+
 generate() {
 	openssl $1 -e -in CRYPTOSAMPLES_KEY256.dat -out CRYPTOSAMPLES_KEY256-$1.enc -K $2 -nopad
 	openssl $1 -d -in CRYPTOSAMPLES_KEY256-$1.enc -out CRYPTOSAMPLES_KEY256-$1.dec -K $2 -nopad
@@ -12,6 +17,7 @@ generate() {
 	echo "--"
 }
 
-generate aes-128-ecb 0123456789ABCEDF
-generate aes-256-ecb 0123456789ABCEDF0123456789ABCEDF
+printf "Kosh: You have always been here." > CRYPTOSAMPLES_KEY256.dat
+generate aes-128-ecb $CRYPTOSAMPLES_KEY128
+generate aes-256-ecb $CRYPTOSAMPLES_KEY256
 
